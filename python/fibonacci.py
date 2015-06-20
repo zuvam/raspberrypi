722a46762f9c29d8161fe38c8d7263a8a1aa09c6
zuvam <zuva.munshi@gmail.com>
2015-06-20 16:26:59 +0200

    added fibonacci.py

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,2 +1,23 @@
 #!/usr/bin/env python3
+# Fun with Fibonacci numbers
+# Fibonacci Sequence: 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, ...
+# F(1) = 1; F(2) = 1
+# F(n) = F(n-1) + F(n-2) , n>2
+def fibonacci(n):
+    assert n > 0
+    if n < 3:
+        return 1
+    else:
+        fm, fn = 1, 1
+        for x in range(2, n): fm, fn = fn, fm + fn
+        return fn
 
+
+if __name__ == '__main__':
+    import sys
+
+    try:
+        n = int(sys.argv[1])
+    except:
+        n = 1
+    print(n, fibonacci(n))
